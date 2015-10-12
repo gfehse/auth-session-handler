dbb85e1b4367997a0124acdf4c0bec8a77823156
gfehse <gfehse@gmail.com>
2015-10-12 14:35:25 -0300

    Adding passport local authentication

--- authServer.js
+++ authServer.js
@@ -1,39 +1,49 @@
 var express = require('express');
 var session = require('express-session');
 var passport = require('passport');
-var Strategy = require('passport-http').BasicStrategy;
+var LocalStrategy = require('passport-local').Strategy;
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 
 
 var app = express();
 app.use(express.static('public'));
-app.use(cookieParser());
-//app.use(bodyParser.json());
+//app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(session({
     secret: 'keyboard cat',
+    name: 'bla',
     resave: false,
     saveUninitialized: false,
-    maxAge: 5000
+    cookie: { secure: false } 
+    //maxAge: 5000
   }));
 app.use(passport.initialize());
 app.use(passport.session());
 //app.use(require('router'));
 
 passport.serializeUser(function(user, done) {
+  console.log('serializando');
+  console.log(user);
   done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done) {
+  console.log('deserializando');
+  console.log(id);
   User.findById(id, function(err, user) {
+    console.log(user);
     done(err, user);
   });
 });
 
 
-passport.use(new Strategy(
+passport.use(new LocalStrategy({
+    'usernameField': 'username',
+    'passwordField': 'password',
+    'session': false
+},
   function(username, password, done) {
-
     if("gustavo" == username && "fehse" == password) {
       return done(null, { id: 'gus', username: 'gustavo', emails: ['gfehse@gmail.com']});
     }
@@ -41,19 +51,19 @@ passport.use(new Strategy(
       return done(null, false, {message:'not logged-in'});
     }
 
+  },
+  function(err) {
+    console.log(err);
   }));
 
 
-app.get('/login',
-  passport.authenticate('basic', { session: false }),
-  function(req, res) {
-
-    res.redirect("/");
-    //res.json({ id: req.user.id, username: req.user.username });
+app.post('/login', 
+  
+  passport.authenticate('local', { 'session': false }),function(req, res) {
+    res.json({status:'ok', 'sessionID' : req});
   });
 
-
-app.get('/', function(req, res) {
+app.get('/', function(req, res, next) {
 
   //response.writeHead(200, { 'Content-Type': 'application/json' });
   console.log(req.user);
